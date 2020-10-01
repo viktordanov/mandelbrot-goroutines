087a1a94827d9ee587871ee4903d64137153425d
Simo Aleksandrov <simo3003@me.com>
2020-10-01 20:34:16 +0300

    Pass filename to writeBytesToImage

--- cmd/main.go
+++ cmd/main.go
@@ -14,18 +14,18 @@ func main() {
 	dimensions := mandelbrot.Size{X: 12000, Y: 8000}
 	canvas := make([]uint8, int(dimensions.X*dimensions.Y)*4)
 	mandelbrot.Mandelbrot(canvas, dimensions, 2000)
-	err := writeBytesToImage(canvas, dimensions)
+	err := writeBytesToImage(canvas, dimensions, "test.png")
 	if err != nil {
 		panic(err)
 	}
 }
 
-func writeBytesToImage(data []uint8, size mandelbrot.Size) error {
+func writeBytesToImage(data []uint8, size mandelbrot.Size, fileName string) error {
 	// Create a blank image 100x200 pixels
 	img := image.NewRGBA(image.Rect(0, 0, int(size.X), int(size.Y)))
 	img.Pix = data
 	// outputFile is a File type which satisfies Writer interface
-	outputFile, err := os.Create("test.png")
+	outputFile, err := os.Create(fileName)
 	if err != nil {
 		// Handle error
 		return err
